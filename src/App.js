af8fd1c5e3fad890e65c48cac4ede6271cadd7ea
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-06 12:51:29 -0700

    pulling data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from "react";
 export default function App() {
   const [count, setCount] = useState(1);
 
+  useEffect(async () => {
+    const person = await fetch("https://swapi.dev/api/people/1/?format=json");
+    const json = await person.json();
+    await console.log(json);
+  }, []);
+
   function clickHandler() {
     setCount((prev) => prev + 1);
   }
